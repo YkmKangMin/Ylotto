87717e73926828b5657721e5001133571d4ea82e
YunKangMin <yunyun28@naver.com>
2018-07-22 23:38:55 +0900

    한글 커밋 내용 테스트

--- src/main/java/com/ykm/ylotto/controller/MainController.java
+++ src/main/java/com/ykm/ylotto/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
 
     @RequestMapping("/hello")
